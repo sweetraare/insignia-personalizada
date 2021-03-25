0b913407bc11c95e7bb7a42f846c6267287c3b47
Alex <ajua_1696@hotmail.com>
2021-03-25 11:45:19 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import insignia from "./insignia.png";
+import grupo from "./grupo.PNG";
+import distrito from "./distrito.PNG";
+import "./App.scss";
 
 function App() {
+  const canvasSize = "400";
   const [nombre, setNombre] = useState("");
   const [canvas, setCanvas] = useState(null);
 
   const canvasRef = useRef(null);
   const imageRef = useRef(null);
-
-  //  useEffect(() => {
-  //    const canvas = canvasRef.current;
-  //    const context = canvas.getContext("2d");
-  //    // setContextReact(context);
-  //    //Our first draw
-  //    context.fillStyle = "#fff";
-  //    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-  //    context.drawImage(imageRef.current, 10, 10);
-  //  }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -27,42 +21,37 @@ function App() {
     //Our first draw
     context.fillStyle = "#fff";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-    context.drawImage(imageRef.current, 10, 10);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    context.globalAlpha = 1;
+    // context.drawImage(imageRef.current, 10, 10);
+    context.drawImage(
+      imageRef.current,
+      0,
+      0,
+      imageRef.current.width,
+      imageRef.current.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
     //Set the font size and the fontface to use.
-    context.font = "25px Arial";
+    context.font = "13px Arial";
 
     //Set the color of the text. This can be
     //an RGB color or a textual description
     //such as red.
-    context.fillStyle = "red";
+    context.fillStyle = "white";
 
     //The X coordinate where to start
-    var x = 50;
+    const x = 137;
 
     //The Y coordinate where to start
-    var y = 100;
+    const y = 275;
 
     //Use the fillText method to draw the text.
     context.fillText(nombre, x, y);
-
-    // if (contextReact) {
-    //  //Set the font size and the fontface to use.
-    //  contextReact.font = "25px Arial";
-
-    //  //Set the color of the text. This can be
-    //  //an RGB color or a textual description
-    //  //such as red.
-    //  contextReact.fillStyle = "255,255,255";
-
-    //  //The X coordinate where to start
-    //  var x = 50;
-
-    //  //The Y coordinate where to start
-    //  var y = 100;
-
-    //  //Use the fillText method to draw the text.
-    //  contextReact.fillText(nombre, x, y);
-    //}
   }, [nombre]);
 
   const handleDownload = () => {
@@ -74,15 +63,34 @@ function App() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={nombre}
-        onChange={(e) => setNombre(e.target.value)}
-      />
-      <img src={logo} ref={imageRef} style={{ display: "none" }} />
-      <button onClick={() => handleDownload()}>Descargar</button>
-      <canvas ref={canvasRef} />
+    <div className="container">
+      <div className="header">
+        <img src={distrito} className="header__logo" />
+        <h1 className="header__text">
+          Gracias por participar en la Ruta de la Cuaresma 2021
+        </h1>
+        <img src={grupo} className="header__logo" />
+      </div>
+      <h4>
+        A continuación, ingresa tu nombre para descargar tu insignia virtual
+      </h4>
+      <div className="controller-container">
+        <label class="input">
+          <input
+            type="text"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            class="input__field"
+            placeholder=" "
+          />
+          <span class="input__label">Nombre</span>
+        </label>
+        <button className="download-button" onClick={() => handleDownload()}>
+          Descargar
+        </button>
+      </div>
+      <img src={insignia} ref={imageRef} style={{ display: "none" }} />
+      <canvas ref={canvasRef} width={canvasSize} height={canvasSize} />
     </div>
   );
 }
